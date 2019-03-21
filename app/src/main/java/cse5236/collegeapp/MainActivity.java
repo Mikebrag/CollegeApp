753cbde630f064b639a68b53193137151c396b59
Mike <mikebrag@roadrunner.com>
2019-03-21 00:37:12 -0400

    Added firebase connection with basic CRUD capabilities

--- app/src/main/java/cse5236/collegeapp/MainActivity.java
+++ app/src/main/java/cse5236/collegeapp/MainActivity.java
@@ -10,6 +10,13 @@ import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.core.view.GravityCompat;
@@ -140,6 +147,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onDestroy() {
         Log.d(TAG, "Entering onDestroy");
         super.onDestroy();
+    }
+
+    public void basicReadWrite() {
+        // [START write_message]
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
+        // [END write_message]
+
+        // [START read_message]
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d(TAG, "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+        // [END read_message]
     }
 
 }
--- app/src/main/java/cse5236/collegeapp/PortfolioFragment.java
+++ app/src/main/java/cse5236/collegeapp/PortfolioFragment.java
@@ -7,10 +7,27 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class PortfolioFragment extends Fragment {
     private static final String TAG = "PortfolioFragment";
+
+    private Button mAddButton;
+    private Button mDeleteButton;
+    private Button mShowButton;
+    private Button mUpdateButton;
+
+
+
 
     @Override
     public void onAttach(Context context) {
@@ -31,6 +48,66 @@ public class PortfolioFragment extends Fragment {
 
         Toast.makeText(getActivity(), R.string.title_activity_portfolio, Toast.LENGTH_SHORT).show();
         View v = inflater.inflate(R.layout.fragment_portfolio, container, false);
+
+        mAddButton = (Button) v.findViewById(R.id.add_button);
+        mDeleteButton = (Button) v.findViewById(R.id.delete_button);
+        mShowButton = (Button) v.findViewById(R.id.show_button);
+        mUpdateButton = (Button) v.findViewById(R.id.update_button);
+        final TextView textView = (TextView) v.findViewById(R.id.simpleTextView);
+
+        mAddButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference myRef = database.getReference("message");
+                myRef.setValue("Note 1");
+            }
+        });
+        mDeleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference myRef = database.getReference("message");
+                myRef.removeValue();
+                textView.setText("");
+            }
+        });
+
+        mShowButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference myRef = database.getReference("message");
+                myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        if (dataSnapshot.exists()) {
+                            String value = dataSnapshot.getValue(String.class);
+                            textView.setText(value);
+                            Log.d("showButton", "Value is: " + value);
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+
+            }
+        });
+
+        mUpdateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference myRef = database.getReference("message");
+                myRef.setValue("Note 2");
+            }
+        });
+
         return v;
     }
 
